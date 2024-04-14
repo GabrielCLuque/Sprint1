33322c6cf71b4f096dad434afc79e960129a87f5
GabrielCLuque <165780394+GabrielCLuque@users.noreply.github.com>
2024-04-14 20:54:21 +0200

    Update ex2.php

--- Ex1/ex2.php
+++ Ex1/ex2.php
@@ -9,6 +9,7 @@
 
 </body>
 <?php 
+//ex1
     $int = 1;
     $dou = 2.2;
     $str = 'a';
@@ -20,13 +21,52 @@
         . $dou . '<br/>'
         . $str . '<br/>'
         . $bol . '<br/>'; 
-
+//EX2
     $str2 = 'Hello, World!';
     $str3 = 'Aquest és el curs de PHP';
         echo strtoupper($str2). '<br/>'
         . strlen($str2). '<br/>'
         . strrev($str2). '<br/>'
-        . $str2 . $str3 ;
-    
+        . $str2 . $str3 . '<br/>';
+
+//EX3
+    $x = 1;
+    $y = 2;
+    $n = 1.1;
+    $m = 2.2;
+
+        echo $x . ' ' . $y . '<br/>'
+        . $x + $y . '<br/>'
+        . $x - $y . '<br/>'
+        . $x * $y . '<br/>'
+        . $x % $y . '<br/>'
+        . $n . ' '. $m . '<br/>'
+        . $n + $m . '<br/>'
+        . $n - $m . '<br/>'
+        . $n * $m . '<br/>'
+        . $n % $m . '<br/>'
+
+        . 'doble variables ' . '<br/>' . $x *2 . '<br/>' . $y *2 . '<br/>' . $n *2 . '<br/>' . $m *2 . '<br/>'
+        . 'producto variables ' . $x + $y + $n + $m . '<br/>' ;
+
+        $numero1= 1; $numero2= 2; $operacion= '+';
+        function Calculadora($numero1, $numero2, $operacion){
+            if ($operacion = '+'){
+                $resultado = $numero1 + $numero2;
+            }
+            else if ($operacion = '-'){
+                $resultado = $numero1 - $numero2;
+            }
+            else if ($operacion = '*'){
+                $resultado = $numero1 * $numero2;
+            }
+            else {
+                $resultado = $numero1 % $numero2;
+            }
+
+            echo $numero1 . ' '. $operacion . ' ' . $numero2 . ' = '. $resultado;
+        }
+        
+        Calculadora($numero1, $numero2, $operacion);
 ?>
 </html>
