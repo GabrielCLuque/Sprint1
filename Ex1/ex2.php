722c0e3c0968d2a8188afb8d0f3544c4d809a9c9
GabrielCLuque <165780394+GabrielCLuque@users.noreply.github.com>
2024-04-14 15:53:52 +0200

    Add files via upload

---
+++ Ex1/ex2.php
@@ -0,0 +1,26 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+
+</body>
+<?php 
+    $int = 1;
+    $dou = 2.2;
+    $str = 'a';
+    $bol = true;
+    define('NOM', 'Gabri');
+    
+    echo  NOM . '<br/>'
+        . $int . '<br/>' 
+        . $dou . '<br/>'
+        . $str . '<br/>'
+        . $bol . '<br/>'; 
+        
+    
+?>
+</html>
