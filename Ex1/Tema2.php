3b96e2f1a30d5f3291ab6ab2f4a6f7c42bde82f9
GabrielCLuque <165780394+GabrielCLuque@users.noreply.github.com>
2024-04-15 01:43:37 +0200

    Update Tema2.php
    ex3

--- Ex1/Tema2.php
+++ Ex1/Tema2.php
@@ -20,5 +20,30 @@ echo count($array) . '<br/>';
 array_splice($array, 0,1);
 //no acabo de entender que me pide el enunciado con 's’han de reorganitzar els seus índexs'
 echo count($array) . '<br/>';
+
+//ex3 
+$palabras= array ('aceituna', 'oliva', 'potencia');
+function Palabrasconletra($listapalabras =array(), $letra){
+//empiezo cada for reseteando respuesta a false
+    $respuesta = false;
+//este for busca por todo el array
+    for($x=0; $x <= count($listapalabras)-1; $x++){
+        $respuesta = false;
+//este for busca cada caracter
+        for($i=0; $i <= strlen($listapalabras[$x])-1; $i++){
+        if ($listapalabras[$x][$i] === $letra){
+            $respuesta = true;
+            }
+        }
+//si respuesta no cambia a true se termina ya quesignifica que hay una palabra sin la letra
+        if ($respuesta == false){
+            break;
+        }
+    }
+
+return $respuesta;
+}
+
+
 ?>
 </html>
