44bc994132f95d809b3933eae75a6bb9e6599a39
GabrielCLuque <165780394+GabrielCLuque@users.noreply.github.com>
2024-04-15 00:01:22 +0200

    Update Tema2.php
    ex2

--- Ex1/Tema2.php
+++ Ex1/Tema2.php
@@ -9,10 +9,16 @@
 
 </body>
 <?php 
+//ex1
 $array = array (1, 2, 3, 4, 5);
 for($z=0; $z <= count($array)-1; $z++){
 echo $array[$z] . '<br/>';
 }
 
+//ex2
+echo count($array) . '<br/>';
+array_splice($array, 0,1);
+//no acabo de entender que me pide el enunciado con 's’han de reorganitzar els seus índexs'
+echo count($array) . '<br/>';
 ?>
 </html>
