9a5d2b192e0e3afb33cdc7f69e34c0ba86c9b73c
GabrielCLuque <gabrielcasasluque@gmail.com>
2024-04-15 10:30:27 +0200

    ex2

--- Ex1/Tema4.php
+++ Ex1/Tema4.php
@@ -48,5 +48,51 @@ class Employee {
     $empleado1->pagarimpostos();
     $empleado2->pagarimpostos();
 
+
+    //EX2
+    class Shape {
+
+      public $ample;
+      public $alt;
+    
+      function set_ample($ample) {
+        $this->ample = $ample;
+      }
+      function set_alt($alt) {
+          $this-> alt = $alt;
+        }
+      function get_ample() {
+        return $this->ample;
+      }
+      function get_alt() {
+        return $this->alt;
+      }
+    }  
+    class rectangle extends Shape{ 
+      function Calculararea(){
+        $area = $this->ample * $this->alt;
+        echo 'L area es de :'. $area . '<br/>';
+      }
+    }
+    class triangle extends Shape{
+        function Calculararea(){
+        $area = ($this->ample * $this->alt) / 2;
+        echo 'L area es de :'. $area . '<br/>';
+      }
+      }
+
+    $triangle1 = new triangle();
+    $rectangle1 = new rectangle();
+
+    $rectangle1->set_ample(2);
+    $rectangle1->set_alt(2);
+
+    $triangle1->set_ample(2);
+    $triangle1->set_alt(2);
+
+    $rectangle1 ->Calculararea();
+    $triangle1 ->Calculararea();
+
+
 ?>
 </html>
