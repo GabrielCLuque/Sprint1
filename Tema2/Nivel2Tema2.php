2ad70cb6dd9f76f798727981c516e7a007092146
GabrielCLuque <gabrielcasasluque@gmail.com>
2024-04-17 13:10:42 +0200

    niv 3

--- Tema2/Nivel2Tema2.php
+++ Tema2/Nivel2Tema2.php
@@ -17,23 +17,10 @@
 
    Totalapagar(7);
 
-   /*
-
-    magina que som a una botiga on es ven:
-
-Xocolata: 1 euro
-Xiclets: 0,50 euros
-Caramels: 1,50 euros
-Implementa un programa que permeti afegir càlculs a un total de compres d'aquest tipus. Per exemple, que si comprem:
-
-2 xocolates, 1 de xiclets i 1 de caramels, tinguem un programa que permeti anar afegint els subtotals a un total, tal que així:
-
-funció(2 xocolates) + funció(1 de xiclets) + funció(1 de carmels) = 2 + 0.5 + 1.5
-
-Sent, per tant, el total, 4.
-    */
 //ex2
-function Calcularpreu(float $Uxocolata,float $Uxiclets,float $Ucarmels):void{
+
+
+
 function Xocolates($Uxocolata):float {
     $preuxocolat = $Uxocolata * 1;
     return $preuxocolat;
@@ -46,9 +33,7 @@ function Carmels($Ucarmels):float {
     $preucarmels = $Ucarmels * 1.50;
     return $preucarmels;
 }
- Xocolates($Uxocolata) ;($Uxiclets); Carmels($Ucarmels); echo $preuxocolat + $preuxiclets + $preucarmels;
-}
-echo '</br>';
-Calcularpreu(2,1,1);
+
+ echo Xocolates(2) . ' + ' . Xiclets(1) . ' + ' . Carmels(1) . ' = ' . Xocolates(2)  +  Xiclets(1) +  Carmels(1) ;
 
 ?>
--- Tema2/Nivel3Tema2.php
+++ Tema2/Nivel3Tema2.php
@@ -1,1 +1,12 @@
 
+<?php 
+function Erastotenescribar($numero){
+    for ($i = 2; $i < $numero; $i++) {
+        if ($i % 2 = 0 ){
+        echo $i 
+        }
+    }
+
+}
+Erastotenescribar(20);
+?>
