92ce4206e6d98339298491a78a0e47eb7d3c2bb5
GabrielCLuque <165780394+GabrielCLuque@users.noreply.github.com>
2024-04-17 13:12:47 +0200

    Update Nivel3Tema2.php

--- Tema2/Nivel3Tema2.php
+++ Tema2/Nivel3Tema2.php
@@ -1,12 +1,32 @@
-
 <?php 
 function Erastotenescribar($numero){
-    for ($i = 2; $i < $numero; $i++) {
-        if ($i % 2 = 0 ){
-        echo $i 
-        }
-    }
+$repeticion = false;
+$listaprimos = array();
+$pl =2;
+$pa = 0;
+function pillarend($array){
 
+}
+
+for ($i = $pl; $i < $numero; $i++) {
+array_push($listaprimos,$i);
+}
+do	{
+for ($x = $pl; $x <= count($listaprimos)-1; $x++) {
+if ($listaprimos[$x] % $pl == 0) {
+unset($listaprimos[$x]);
+}
+}
+if ( ($listaprimos[$pa] * $listaprimos[$pa]) < $numero ) {
+    $pl= $listaprimos[$pa];
+    $pa++;
+}
+else {
+    $repeticion = true;
+}
+
+} while ($repeticion == false);
+print_r($listaprimos). '</br>';
 }
 Erastotenescribar(20);
 ?>
