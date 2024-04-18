c29937d6ce9dd60260320f5ec094f2faf7f802d2
GabrielCLuque <gabrielcasasluque@gmail.com>
2024-04-18 11:15:28 +0200

    nivel 3 t1 corregido

--- Tema3/Nivel1Tema3.php
+++ Tema3/Nivel1Tema3.php
@@ -10,7 +10,7 @@
 </body>
 <?php 
 //ex1
-$array = array (1, 2, 3, 4, 5);
+$array = array (0 =>1, 2, 3, 4, 5);
 for($z=0; $z <= count($array)-1; $z++){
 echo $array[$z] . '<br/>';
 }
@@ -18,37 +18,31 @@ echo $array[$z] . '<br/>';
 //ex2
 echo count($array) . '<br/>';
 array_splice($array, 0,1);
-//no acabo de entender que me pide el enunciado con s’han de reorganitzar els seus índexs'
 echo count($array) . '<br/>';
 
 //ex3 
 $palabras= array ('aceituna', 'oliva', 'potencia');
 function Palabrasconletra($listapalabras =array(), $letra){
-//empiezo cada for reseteando respuesta a false
-    $respuesta = false;
-//este for busca por todo el array
+
+//Simplificado
     for($x=0; $x <= count($listapalabras)-1; $x++){
         $respuesta = false;
-//este for busca cada caracter
-        for($i=0; $i <= strlen($listapalabras[$x])-1; $i++){
-        if ($listapalabras[$x][$i] === $letra){
-            $respuesta = true;
-            }
+        $respuesta = stristr($listapalabras[$x],$letra);
+        if($respuesta==true){
+            echo $listapalabras[$x] . '<br/>';
         }
-//si respuesta no cambia a true se termina ya quesignifica que hay una palabra sin la letra
-        if ($respuesta == false){
-            break;
-        }
-    }
-
-return $respuesta;
 }
-
+}
+ Palabrasconletra($palabras, 'p');
 
 
 //ex4 
 
 $yoliteral = ['Nombre' => 'Gabri','Edad' => 24,'Mail' => 'gabrielcasasluque@gmail.com', 'Platofavorito' => 'Carbonara'];
+
+?>
+</html>
+
 
 ?>
 </html>
